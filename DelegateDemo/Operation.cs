d39f83c923391700fb645eca5c4be06e329f6eb6
SumitomShome <sumitomshome@gmail.com>
2020-11-29 23:06:17 +0530

    [Sumitom] Add: Method to use delegate

---
+++ DelegateDemo/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Delegate
+{
+    class Operation
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("A+B" + " " + (a + b));
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine("A*B" + " " +(a * b));
+        }
+    }
+}
---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Delegate
+{
+    public delegate void MyDelegate(string msg); 
+    class Program
+    {
+        public delegate void MyDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            MyDelegate[] delegates =
+            {
+                new MyDelegate(Operation.Add),
+                new MyDelegate(Operation.Mul)
+            };
+            for(int i = 0; i< delegates.Length; i++)
+            {
+                delegates[i](2,2);
+                delegates[i](4,4);
+            }
+            Console.ReadKey();
+        }
+    }
+}
